4efb12a4df005e65e2e638da12a0f7ac20e82a74
Justine-May <justineestacio08@gmail.com>
2025-11-26 15:10:32 +0800

    initial commit

---
+++ inc_metadata.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>Template</title>
+	<?php include ("inc_metadata.php"); ?>
+</head>
+
+<body>
+
+<?php include ("inc_header.php"); ?>
+
+<h1 align="center">Headline</h1>
+
+
+
+<?php include ("inc_footer.php"); ?>
+
+</body>
+</html>
+
